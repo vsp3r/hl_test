978ed40b228231668063580430c636c345bc741a
sank <sanketvaja@berkeley.edu>
2023-09-21 18:29:53 -0700

    user_state and open_orders

---
+++ connect_test.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import sys
+import time
+
+from hyperliquid.info import Info
+from hyperliquid.utils import constants
+addy = '0x40574F1bcA0C9054C3617c117cE9A13D6aFb882F'
+info = Info(constants.TESTNET_API_URL, skip_ws=True)
+user_state = info.user_state(addy)
+
+print(info.open_orders(addy))
+# info.send_order()
