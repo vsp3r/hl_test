2bff85d6cbbddab6f1f0081408b64b6132587b86
vsp3r <sanketvaja@berkeley.edu>
2023-09-22 01:38:29 +0000

    stuff

--- connect_test.py
+++ connect_test.py
@@ -7,7 +7,8 @@ from hyperliquid.info import Info
 from hyperliquid.utils import constants
 addy = '0x40574F1bcA0C9054C3617c117cE9A13D6aFb882F'
 info = Info(constants.TESTNET_API_URL, skip_ws=True)
-user_state = info.user_state(addy)
+user_state = info.user_state(addy)["assetPositions"][9]
 
-print(info.open_orders(addy))
+# print(info.open_orders(addy))
+print(user_state)
 # info.send_order()
